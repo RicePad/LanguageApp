bc9c6350278bea67f3b7bcefc900baf4c65000a6
JON <jonn.thinkrtc@gmail.com>
2018-08-04 16:06:53 -0400

    Added Intent classes on mainactivity.java

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.miwok;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -30,20 +32,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openNumberList(View view){
-
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
     }
 
     public void openFamilyList(View view){
-
+        Intent i = new Intent(this, FamilyAcitivity.class);
+        startActivity(i);
 
     }
 
     public void openColorList(View view){
-
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
 
     }
 
     public void openPhrasesList(View view){
+        Intent i = new Intent(this, PhrasesActivity.class);
+        startActivity(i);
 
     }
 
